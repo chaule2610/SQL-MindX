27426ef5bac9419415bb4cee847dd75c41483271
Chau Le <120628995+chaule2610@users.noreply.github.com>
2023-12-06 13:31:19 +0700

    Update Minh Chau-BTVN buoi 2.sql

--- Minh Chau-BTVN buoi 2.sql
+++ Minh Chau-BTVN buoi 2.sql
@@ -37,25 +37,6 @@ WHERE
 	YEAR(TransactionDate)=2014
 )
 
---- 3. Tạo ra một cột mới từ bảng Transactionhistory có tên là Quy
---- Nếu MONTH từ 1-3 là Quy 1, Nếu MONTH từ 4-5 là Quy 2, Nếu MONTH từ 7-9 là Quy 3, Còn lại là Q4
---- Sau đó tính value (quantity*actual cost) theo từng quý + năm
---- Kết quả yêu cầu: IN ra tổng value theo năm của Quý cao nhất
-
-
-
---- Bước 1: tạo 1 cột tên Quý
-ALTER TABLE transactionhistory 
-ADD Quy Char(2)
-
-UPDATE transactionhistory 
-SET Quy =
-CASE 
-	WHEN MONTH(TransactionDate) BETWEEN 1 AND 3 THEN 'Q1' 
-	WHEN MONTH(TransactionDate) BETWEEN 4 AND 6 THEN 'Q2' 
-	WHEN MONTH (TransactionDate) BETWEEN 7 AND 9 THEN 'Q3' 
-	ELSE 'Q4' 
-END
 
 
 
